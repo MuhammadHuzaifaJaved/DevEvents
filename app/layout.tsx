10ec9f005d239047ac625c73a6304f2780169155
MuhammadHuzaifaJaved <mhuzaifajaved07@gmail.com>
2026-03-17 14:59:40 +0500

    Implement PostHog

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 import { cn } from "@/lib/utils";
 import LightRays from "@/components/LightRays";
 import Navbar from "@/components/Navbar";
+import { PostHogProvider } from "./providers";
+import PostHogPageView from "./PostHogPageView";
+import { Suspense } from "react";
 
 const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
@@ -32,28 +35,32 @@ export default function RootLayout({
       <body
         className={`${schibstedGrotesk.variable} ${martianMono.variable} min-h-screen antialiased`}
       >
-         <Navbar />
-        <div className="absolute inset-0 top-0 z-[-1] min-h-screen">
-        <LightRays
-        raysOrigin="top-center-offset"
-        raysColor="#5dfeca"
-        raysSpeed={0.5}
-        lightSpread={0.9}
-        rayLength={1.4}
-        followMouse={true}
-        mouseInfluence={0.1}
-        noiseAmount={0}
-        distortion={0}
-        className="custom-rays"
-        pulsating={false}
-        fadeDistance={1}
-        saturation={1}
-      /> 
-      </div>
-      <main>
-       
-        {children}
-        </main>
+        <PostHogProvider>
+          <Suspense fallback={null}>
+            <PostHogPageView />
+          </Suspense>
+          <Navbar />
+          <div className="absolute inset-0 top-0 z-[-1] min-h-screen">
+            <LightRays
+              raysOrigin="top-center-offset"
+              raysColor="#5dfeca"
+              raysSpeed={0.5}
+              lightSpread={0.9}
+              rayLength={1.4}
+              followMouse={true}
+              mouseInfluence={0.1}
+              noiseAmount={0}
+              distortion={0}
+              className="custom-rays"
+              pulsating={false}
+              fadeDistance={1}
+              saturation={1}
+            />
+          </div>
+          <main>
+            {children}
+          </main>
+        </PostHogProvider>
       </body>
     </html>
   );
